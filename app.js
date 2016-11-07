728b56b7cd8a315dc0ad395e349e7ee3f43bfea3
Pedro Alves <3pedru@gmail.com>
2016-11-07 01:57:51 -0200

    test deploy on heroku, again

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let express = require('express'),
     url = 'mongodb://localhost:27017/cookbook'
 
 if (process.env.MONGODB_URI) {
-    url = 'mongodb://heroku_cmbkjpkr:q7i1u0lhd9n6m542b6g9igj8jc@ds145997.mlab.com:45997/heroku_cmbkjpkr'
+    url = MONGODB_URI
 }
 
 process.db = db;
@@ -15,8 +15,6 @@ process.db = db;
 load('routes', {
     cwd: 'app'
 }).then('mongo').into(app)
-
-console.log(process.env.MONGODB_URI, url);
 
 db.connect(url, function(err) {
     if (err) {
