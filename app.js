b0db276098a48a43cde4d8de86caec9e2f78ad35
Pedro Alves <3pedru@gmail.com>
2016-11-07 01:51:12 -0200

    test deploy on heroku, url to mongo connection

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 let express = require('express'),
     app = express(),
     load = require('express-load'),
-    db = require('./app/mongo/mongoConnection')
+    db = require('./app/mongo/mongoConnection'),
+    url = 'mongodb://localhost:27017/cookbook'
+
+if (process.MONGODB_URI) {
+    url = 'mongodb://heroku_cmbkjpkr:q7i1u0lhd9n6m542b6g9igj8jc@ds145997.mlab.com:45997/heroku_cmbkjpkr'
+}
 
 process.db = db;
 
@@ -11,8 +16,9 @@ load('routes', {
     cwd: 'app'
 }).then('mongo').into(app)
 
-//db.connect('mongodb://localhost:27017/cookbook', function(err) {
-db.connect('mongodb://heroku_cmbkjpkr:q7i1u0lhd9n6m542b6g9igj8jc@ds145997.mlab.com:45997/heroku_cmbkjpkr', function(err) {
+console.log(process.MONGODB_URI, url);
+
+db.connect(url, function(err) {
     if (err) {
         console.log('Unable to connect to Mongo.')
         process.exit(1)
