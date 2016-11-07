da1ccfac06dd5e4d9cd1d1e41551c8431aee033f
Pedro Alves <3pedru@gmail.com>
2016-11-07 01:20:38 -0200

    test deploy on heroku

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ load('routes', {
     cwd: 'app'
 }).then('mongo').into(app)
 
-db.connect('mongodb://localhost:27017/cookbook', function(err) {
+//db.connect('mongodb://localhost:27017/cookbook', function(err) {
+db.connect('mongodb://heroku_cmbkjpkr:q7i1u0lhd9n6m542b6g9igj8jc@ds145997.mlab.com:45997/heroku_cmbkjpkr', function(err) {
     if (err) {
         console.log('Unable to connect to Mongo.')
         process.exit(1)
